bbe647ab64bfdf67737415d63ce886f3dd1f7ab6
samuel-pedraza <samdpedraza@gmail.com>
2019-02-10 01:04:14 -0800

    add GA

--- header.php
+++ header.php
@@ -2,5 +2,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>WP Dev - In-Depth Learning Around WordPress Development</title>
 	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133229397-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-133229397-1');
+	</script>
+
 	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
 </head>
